5ba464f3a8bc885dafaf6b7fc6e6c3b0c412a84b
Aditya Permadi <adityaperm06@gmail.com>
2022-02-25 00:11:25 +0700

    fix data type, migration table

--- test/users_test.go
+++ test/users_test.go
@@ -1,5 +1,48 @@
 package test
 
-func GetConnection() {
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"survorest/helper"
+	"survorest/user"
+	"testing"
+)
+
+func GetConnection() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/survo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func MigrateTable(db *gorm.DB) {
+	if db.Migrator().HasTable("users") {
+		db.Migrator().DropTable("users")
+		log.Printf("Table users dropped")
+		return
+	}
+	db.Migrator().CreateTable(&user.User{})
+
+	fmt.Println("Migrate Success")
+}
+func TruncateTable(db *gorm.DB) {
+	db.Exec("TRUNCATE users")
+}
+
+func TestMigrateTableUser(t *testing.T) {
+	db, err := GetConnection()
+	helper.ErrorNotNil(err)
+
+	MigrateTable(db)
+	assert.Equal(t, true, db.Migrator().HasTable("users"))
+	assert.NoError(t, err)
+}
+
+func TestCreate(t *testing.T) {
 
 }
--- user/entity.go
+++ user/entity.go
@@ -1,14 +1,14 @@
 package user
 
 type User struct {
-	Id         int64  `json:"id"`
-	FullName   string `json:"fullName"`
-	Email      string `json:"email"`
-	Username   string `json:"username"`
-	Occupation string `json:"occupation"`
-	Role string `json:"role"`
-	Password   string `json:"password"`
-	Image      string `json:"image"`
-	Phone string `json:"phone"`
-	Birthday string `json:"birthday"`
+	Id         int64  `json:"id" gorm:"primaryKey; not null"`
+	FullName   string `json:"fullName" gorm:"type:varchar(100); not null"`
+	Email      string `json:"email" gorm:"type:varchar(100); not null"`
+	Username   string `json:"username"  gorm:"type:varchar(100); not null"`
+	Occupation string `json:"occupation" gorm:"type:varchar(100); not null"`
+	Role       string `json:"role" gorm:"type:varchar(100); not null"`
+	Password   string `json:"password" gorm:"type:varchar(100); not null"`
+	Image      string `json:"image" gorm:"type:varchar(100); not null"`
+	Phone      string `json:"phone" gorm:"type:varchar(100); not null"`
+	Birthday   string `json:"birthday" gorm:"type:varchar(100); not null"`
 }
